f05d0e1a9e5638072d96bd2dfa28af497a0e2f0e
Miguel Sánchez <miguelsl0927@gmail.com>
2024-05-07 06:55:23 -0500

    Encapsulamiento -Implementación de Métodos para Manipulación de Propiedades por Miguel Sánchez

--- src/Persona.java
+++ src/Persona.java
@@ -1,5 +1,35 @@
 public class Persona {
-    private String Nombre;
+    private String nombre;
     private String fechaNacimiento;
-    private Integer Edad;
+    private Integer edad;
+
+    public Persona(String nombre, String fechaNacimiento, Integer edad) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.edad = edad;
+    }
+
+    public void establecerNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String obtenerNombre(){
+        return this.nombre;
+    }
+
+    public void establecerEdad(Integer edad){
+        this.edad = edad;
+    }
+
+    public Integer obtenerEdad(){
+        return this.edad;
+    }
+
+    public void establecerFechaNacimiento(String fechaNacimiento){
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String ObtenerFechaNacimiento(){
+        return this.fechaNacimiento;
+    }
 }

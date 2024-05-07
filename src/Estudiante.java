ba92df07483784025b7ebbfc0b20deed8ba01dd5
Miguel Sánchez <miguelsl0927@gmail.com>
2024-05-07 08:32:15 -0500

    Herencia -Implementación de Propiedad adicional para el grado por Miguel Sánchez

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,5 +1,7 @@
 public class Estudiante extends Persona {
-    public Estudiante(String nombre, String fechaNacimiento, Integer edad) {
+    private String grado;
+    public Estudiante(String nombre, String fechaNacimiento, Integer edad, String grado) {
         super(nombre, fechaNacimiento, edad);
+        this.grado = grado;
     }
 }

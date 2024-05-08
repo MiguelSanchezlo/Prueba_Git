af74495c349510dff492c14b60d5149979e4a60c
Miguel Sánchez <miguelsl0927@gmail.com>
2024-05-07 20:42:23 -0500

    Herencia - Implementación de Constructores porMigueÃl Sánchez

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,8 +1,14 @@
 public class Estudiante extends Persona {
     private String grado;
+
+    // Constructor propio de la clase
     public Estudiante(String nombre, String fechaNacimiento, Integer edad, String grado) {
         super(nombre, fechaNacimiento, edad);
         this.grado = grado;
+    }
+    // Constructor heredado
+    public Estudiante(String nombre, String fechaNacimiento, Integer edad) {
+        super(nombre, fechaNacimiento, edad);
     }
 
     @Override

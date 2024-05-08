5751bbbeca4644967fdd9c50e07c5ad61e800a81
Miguel Sánchez <miguelsl0927@gmail.com>
2024-05-07 22:06:52 -0500

    Polimorfismo - Implementación en Clase Empleado por Miguel Sánchez

---
+++ src/Empleado.java
@@ -0,0 +1,12 @@
+public class Empleado extends Persona implements Trabajador{
+    private String cargo;
+
+    public Empleado(String nombre, String fechaNacimiento, Integer edad) {
+        super(nombre, fechaNacimiento, edad);
+    }
+
+    @Override
+    public void realizarTarea() {
+        System.out.println("Realizando tarea laboral.");
+    }
+}

0b0d270ffb5cc8870c01a4b5cb14963c96e1d08e
Miguel Sánchez <miguelsl0927@gmail.com>
2024-05-08 22:23:34 -0500

    Herencia y Polimorfismo - Implementación de Subclases por Miguel Sánchez

---
+++ src/Gerente.java
@@ -0,0 +1,5 @@
+public class Gerente extends Empleado{
+    public Gerente(String cargo, String nombre, Integer salario) {
+        super(cargo, nombre, salario);
+    }
+}
---
+++ src/Vendedor.java
@@ -0,0 +1,5 @@
+public class Vendedor extends Empleado{
+    public Vendedor(String cargo, String nombre, Integer salario) {
+        super(cargo, nombre, salario);
+    }
+}

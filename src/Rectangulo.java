fe92e10d912764acaac46d3c5d927e954456fa1f
Miguel Sánchez <miguelsl0927@gmail.com>
2024-05-08 22:10:31 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Cálculo de Área y Perímetro po Miguel Sánchez

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,9 +1,18 @@
 public class Rectangulo extends FiguraGeometrica{
     private Integer lados;
-    private Integer longitud;
+    private Integer altura;
     private Integer ancho;
 
     public Rectangulo(String nombre, String color) {
         super(nombre, color);
+    }
+
+    public Integer areaRectangulo() {
+        return (altura * ancho);
+    }
+
+    public Integer perimetroRectangulo() {
+        Integer perimetro = (2 * altura) + (2 * ancho);
+        return perimetro;
     }
 }
--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,9 +1,26 @@
 public class Triangulo extends FiguraGeometrica{
-    private Integer lados;
-    private Integer longitud;
-    private Integer ancho;
+    private Float lado1;
+    private Float lado2;
+    private Float lado3;
+    private Float altura;
+    private Float ancho;
 
-    public Triangulo(String nombre, String color) {
+    public Triangulo(String nombre, String color, Float lado1, Float lado2, Float lado3, Float altura, Float ancho) {
         super(nombre, color);
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+        this.altura = altura;
+        this.ancho = ancho;
+    }
+
+    public Float areaTriangulo (Float altura, Float ancho) {
+        Float area = (ancho * altura) / 2;
+        return area;
+    }
+
+    public Float perimetroTriangulo (Float lado1, Float lado2, Float lado3) {
+        Float perimetro = lado1 + lado2 + lado3;
+        return perimetro;
     }
 }

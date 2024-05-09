67076063926a4639cb769ba3ab884afaaabd507c
Miguel Sánchez <miguelsl0927@gmail.com>
2024-05-08 22:39:57 -0500

    Herencia y Polimorfismo - Implementación del Método calcularSalario() en Cla Gerente por Miguel Sánchez

--- src/Gerente.java
+++ src/Gerente.java
@@ -2,4 +2,11 @@ public class Gerente extends Empleado{
     public Gerente(String cargo, String nombre, Float salario) {
         super(cargo, nombre, salario);
     }
+
+    @Override
+    public Float calcularSalario() {
+        Float salario = 1500000F;
+        Float bono = 250000F;
+        return salario + bono;
+    }
 }
